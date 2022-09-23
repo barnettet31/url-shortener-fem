152ba0c88c16ad81248f5adfb65ae11d1a23cbc5
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-23 16:56:31 -0400

    added onclick for mobile nav

--- src/components/hamburger/hamburger.component.tsx
+++ src/components/hamburger/hamburger.component.tsx
@@ -1,12 +1,15 @@
-export const Hamburger = () => {
+export const Hamburger = ({onClick}:{
+  onClick:()=>void;
+} ) => {
   return (
     <svg
+      onClick={onClick}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-6 h-6 lg:hidden"
     >
       <path
         strokeLinecap="round"
