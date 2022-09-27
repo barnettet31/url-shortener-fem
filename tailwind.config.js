f3ccda67257e625e91bdfa7e5a323ef7893b30d4
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-27 16:52:13 -0400

    added background image to tailwind config for easier use

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     extend: {
       fontFamily:{
         'sans':['Poppins', ...defaultTheme.fontFamily.sans]
+      },
+      backgroundImage:{
+        'mobile-shorten':`url('../public/images/bg-shorten-mobile.svg')`,
+        'desktop-shorten':"url('../public/images/bg-shorten-desktop.svg')"
       }
     },
   },
