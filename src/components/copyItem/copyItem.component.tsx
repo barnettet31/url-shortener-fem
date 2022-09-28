d2353a0b49b1e362963ab0027df41c4f7bd09f93
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-28 19:06:00 -0400

    created component for the copy items

---
+++ src/components/copyItem/copyItem.component.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+interface ICopyItemProps {
+  originalLink: string;
+  shortenedLink: string;
+}
+export const CopyItem = ({ originalLink, shortenedLink }: ICopyItemProps) => {
+  const [isCopied, setIsCopied] = useState(false);
+  const handleCopyClick = async (string: string) => {
+    if (isCopied) return;
+    if ("clipboard" in navigator) {
+      await navigator.clipboard.writeText(shortenedLink);
+      setTimeout(() => setIsCopied(false), 5000);
+      return setIsCopied(true);
+    }
+  };
+  return (
+    <div className="flex flex-col justify-between px-4 bg-white lg:flex-row rounded-[10px] gap-4 py-4 flex-start lg:items-center">
+      <p className="text-black border-b-1 border-b-slate-400">{originalLink}</p>
+      <div className="flex flex-col items-start gap-4 lg:items-center lg:flex-row">
+        <p className="text-[#2bd0d0]">{shortenedLink}</p>
+        <div
+          onClick={() => handleCopyClick(shortenedLink)}
+          className={`rounded-[5px] py-2 text-white text-center px-4 self-center w-full transition-all ease-in-out ${isCopied ?'bg-[#3A3054]':'bg-[#2bd0d0] cursor-pointer hover:bg-[#9ae3e3]'}`}
+        >
+          {isCopied? 'Copied!':'Copy'}
+        </div>
+      </div>
+    </div>
+  );
+};
