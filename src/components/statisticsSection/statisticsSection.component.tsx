ecdd71eeca28ad25c081861c4c4f2bf2901d0b1d
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-09-29 19:50:01 -0400

    added styling to the features cards

--- src/components/statisticsSection/statisticsSection.component.tsx
+++ src/components/statisticsSection/statisticsSection.component.tsx
@@ -17,9 +17,9 @@ export const StatisticsSection = () => {
               className="w-24px"
             />
           </div>
-          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8">
-            <h3 className="text-[#3a3054]">Brand Recognition</h3>
-            <p>
+          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8 text-center lg:text-left">
+            <h3 className="text-[#3a3054] text-[22px] mb-3">Brand Recognition</h3>
+            <p className="text-[#9E9AA8] text-[15px] font-medium">
               Boost your brand recognition with each click. Generic links don’t
               mean a thing. Branded links help instil confidence in your
               content.
@@ -29,34 +29,30 @@ export const StatisticsSection = () => {
         <div className="flex flex-col w-[327px] h-[289px] lg:w-[350px] lg:h-[311px] relative">
           <div className=" absolute -top-[44px] left-[120px] lg:left-[53px] w-[88px] h-[88px] rounded-full bg-[#3A3054] flex items-center justify-center">
             <img
-              src={`${process.env.PUBLIC_URL} ./images/icon-brand-recognition.svg`}
+              src={`${process.env.PUBLIC_URL} ./images/icon-detailed-records.svg`}
               alt=""
               className="w-24px"
             />
           </div>
-          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8">
-            <h3 className="text-[#3a3054]">Brand Recognition</h3>
-            <p>
-              Boost your brand recognition with each click. Generic links don’t
-              mean a thing. Branded links help instil confidence in your
-              content.
+          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8 text-center lg:text-left">
+            <h3 className="text-[#3a3054] text-[22px] mb-3">Detailed Records</h3>
+            <p className="text-[#9E9AA8] text-[15px] font-medium">
+            Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.
             </p>
           </div>
         </div>
         <div className="flex flex-col w-[327px] h-[289px] lg:w-[350px] lg:h-[311px] relative">
           <div className=" absolute -top-[44px] left-[120px] lg:left-[53px] w-[88px] h-[88px] rounded-full bg-[#3A3054] flex items-center justify-center">
             <img
-              src={`${process.env.PUBLIC_URL} ./images/icon-brand-recognition.svg`}
+              src={`${process.env.PUBLIC_URL} ./images/icon-fully-customizable.svg`}
               alt=""
               className="w-24px"
             />
           </div>
-          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8">
-            <h3 className="text-[#3a3054]">Brand Recognition</h3>
-            <p>
-              Boost your brand recognition with each click. Generic links don’t
-              mean a thing. Branded links help instil confidence in your
-              content.
+          <div className="w-full bg-white pt-[77px] rounded-[5px] px-8 pb-8 text-center lg:text-left">
+            <h3 className="text-[#3a3054] text-[22px] mb-3">Fully Customizable</h3>
+            <p className="text-[#9E9AA8] text-[15px] font-medium">
+            Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.
             </p>
           </div>
         </div>
