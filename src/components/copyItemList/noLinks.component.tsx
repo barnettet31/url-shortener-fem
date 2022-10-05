de2e238c0e91cb28bad9a4d71ba3c32da46db303
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-10-05 18:57:57 -0400

    updated text on noLinks

--- src/components/copyItemList/noLinks.component.tsx
+++ src/components/copyItemList/noLinks.component.tsx
@@ -1,6 +1,6 @@
 export const  NoLinks = ()=>{
     return(
       <div className="flex flex-col justify-between px-4 bg-white lg:flex-row rounded-[10px] gap-4 py-4 flex-start lg:items-center">
-  <h3 className="mx-auto">Your decoded links will apear here.</h3></div>
+  <h3 className="mx-auto">Your shortened links will apear here.</h3></div>
     )
   }
